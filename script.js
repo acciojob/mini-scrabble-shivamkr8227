bb70e30fe4ed0f16700b6835f3933a3690037a5e
shivamkr8227 <63731531+shivamkr8227@users.noreply.github.com>
2023-05-19 16:01:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your code here
+function countLetters() {
+    const input = document.getElementById("evaluatedText").value;
+    const letterCount = input.length;
+    document.getElementById("letterCount").textContent = letterCount;
+}
